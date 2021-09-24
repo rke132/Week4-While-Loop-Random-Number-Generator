4e1434678e346ce94b3a07f21937424372cebba6
Julia Aasmaa <my_e_10@hotmail.com>
2021-09-24 14:51:50 +0300

    Week 4 projects

---
+++ GuessingGameInfinite/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GuessingGameInfinite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis ta on mängu võitnud;
+            //katsete arv on piiramatu.
+            //*programm genereerib juhuslikku numbrit ühte korda.
+
+            Random rnd = new Random();
+            int cpuNumber = rnd.Next(1, 11);
+
+            while ()
+            {
+
+            }
+
+        }
+    }
+}
---
+++ GuessingGameThreeTries/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GuessingGameThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm genereerib juhuslikku numbrit 1 kuni 10;
+            //kasutaja peab seda numbrit ära arvama;
+            //kui kasutaja suutis seda numbrit ära arvata, siis ta on mängu võitnud;
+            //kasutajal on kolm katset, kui kasutaja ei ole suutnud kolme kastega numbrit ära arvata
+            //siis mängu võidab arvuti.
+            //*programm genereerib juhuslikku numbrit ühte korda.
+        }
+    }
+}
